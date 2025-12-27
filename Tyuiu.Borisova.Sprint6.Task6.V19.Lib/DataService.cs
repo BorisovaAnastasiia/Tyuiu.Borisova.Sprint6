c4aeab99ed3f1b07c28b7c7c61e925cbabbf164b
Anastasiia Borisova <kuqhoxp@gmail.com>
2025-12-27 08:20:04 +0500

    Task2

---
+++ Tyuiu.Borisova.Sprint6.Task6.V19.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.Borisova.Sprint6.Task6.V19.Lib
+{
+    public class DataService : ISprint6Task6V19
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string resStr = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                string[] temp;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    temp = line.Split(' ');
+                    foreach (string item in temp)
+                    {
+                        if (item.Contains('l')) resStr += item + " ";
+                    }
+
+                }
+            }
+            return resStr;
+        }
+    }
+}
---
+++ Tyuiu.Borisova.Sprint6.Task6.V19.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.Borisova.Sprint6.Task6.V19.Lib;
+
+namespace Tyuiu.Borisova.Sprint6.Task6.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCollectTextFromFile()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\user\source\repos\Tyuiu.ChepurnykhSR.Sprint6\InPutFileTask6V19.txt";
+            string res = ds.CollectTextFromFile(path);
+            string wait = "HqrXl PAlR TwlB AWkgYPxilmC ";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
